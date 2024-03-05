871d66eb736160cf05112ce2991fe32b2df83a2c
blm <bruce.mclean@unfoldingword.org>
2024-03-05 16:09:03 -0500

    Get messaging working

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -1,18 +1,24 @@
 import { vscode } from "./utilities/vscode";
 import { useState } from 'react'
-import { VSCodeButton } from "@vscode/webview-ui-toolkit/react";
+import { VSCodeButton, VSCodeTextField } from "@vscode/webview-ui-toolkit/react";
 import "./App.css";
 
 function App() {
   const [recording, setRecording] = useState<boolean>(false)
+  const [value, setValue] = useState('');
 
   function handleRecordClick() {
     setRecording(true)
-    console.log("handleRecordClick() - Recording Now! 🤠")
+    console.log(`handleRecordClick() - ${value}`)
     vscode.postMessage({
       command: "extension.startRecording",
       text: "Recording Now! 🤠",
     });
+  }
+
+  function handleTextChange(text:string) {
+    setValue(text)
+    console.log(`handleTextChange() - ${text}`)
   }
 
   function handleStopClick() {
@@ -28,15 +34,16 @@ function App() {
 
   return (
       recording ?
-          <main>
-            <h1>Speak Now!</h1>
-            <VSCodeButton onClick={handleStopClick}>Stop</VSCodeButton>
-          </main>
-      :
-          <main>
-            <h1>Recording!</h1>
-            <VSCodeButton onClick={handleRecordClick}>Record</VSCodeButton>
-          </main>
+        <main>
+          <VSCodeButton onClick={handleStopClick}>Stop</VSCodeButton>
+          <h1>Recording!</h1>
+        </main>
+        :
+        <main>
+          <h1>Speak Now!</h1>
+          <VSCodeTextField value={value} onInput={e => handleTextChange(e.target?.value || '')} />
+          <VSCodeButton onClick={handleRecordClick}>Record</VSCodeButton>
+        </main>
   )
 }
 
